d0fb2267395f91b164f5bd9bf7696aa5b3f86092
t-nakane <je_pense_donc_je_suis_honor@yahoo.co.jp>
2018-09-15 21:30:17 +0900

    modify war.py along Part.6

--- war.py
+++ war.py
@@ -16,20 +16,14 @@ class Card:
         if self.value < c2.value:
             return True
         if self.value == c2.value:
-            if self.suit < c2.suit:
-                return True
-            else:
-                return False
+            return self.suit < c2.suit
         return False
     
     def __gt__(self, c2):
         if self.value > c2.value:
             return True
         if self.value == c2.value:
-            if self.suit > c2.suit:
-                return True
-            else:
-                return False
+            return self.suit > c2.suit
         return False
     
     def __repr__(self):
@@ -45,7 +39,7 @@ class Deck:
                 self.cards.append(Card(i, j))
         shuffle(self.cards)
     
-    def rm_card(self):
+    def draw(self):
         if len(self.cards) == 0:
             return
         return self.cards.pop()
@@ -64,15 +58,13 @@ class Game:
         self.p1 = Player(name1)
         self.p2 = Player(name2)
     
-    def wins(self, winner):
+    def print_winner(self, winner):
         w = "{} win this round!"
-        w = w.format(winner)
-        print(w)
+        print(w.format(winner.name))
     
-    def draw(self, p1n, p1c, p2n, p2c):
+    def print_draw(self, p1, p2):
         d = "{} draws {}\n{} draws {}"
-        d = d .format(p1n, p1c, p2n, p2c)
-        print(d)
+        print(d.format(p1.name, p1.card, p2.name, p2.card))
     
     def play_game(self):
         cards = self.deck.cards
@@ -84,20 +76,18 @@ class Game:
             if response == "q":
                 break
             
-            p1c = self.deck.rm_card()
-            p2c = self.deck.rm_card()
-            p1n = self.p1.name
-            p2n = self.p2.name
-            self.draw(p1n, p1c, p2n, p2c)
-            if p1c > p2c:
+            self.p1.card = self.deck.draw()
+            self.p2.card = self.deck.draw()
+            self.print_draw(self.p1, self.p2)
+            if self.p1.card > self.p2.card:
                 self.p1.wins += 1
-                self.wins(self.p1.name)
+                self.print_winner(self.p1)
             else:
                 self.p2.wins += 1
-                self.wins(self.p2.name)
+                self.print_winner(self.p2)
         
         win = self.winner(self.p1, self.p2)
-        print("The end of game. Winner is {}!".format(win))
+        print("The game is over. Winner is {}!".format(win))
 
     def winner(self, p1, p2):
         if p1.wins > p2.wins:
